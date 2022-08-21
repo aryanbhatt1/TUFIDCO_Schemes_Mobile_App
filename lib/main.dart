f500858f55fd99702f165891f2e13b196f563f71
Aryan Bhatt <aryanbhatt1002>
2022-08-21 17:13:28 +0530

    Added Upload Option WebView

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_pro/platform_interface.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
 import 'package:provider/provider.dart';
 import 'backevent_notifier.dart';
 import 'package:flutter/services.dart';
