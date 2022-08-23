a381c51c145d960b71d9a308e4bbf9d1761dea9c
Aryan Bhatt <aryanbhatt1002>
2022-08-24 02:30:08 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_webview_pro/webview_flutter.dart';
 import 'package:provider/provider.dart';
 import 'backevent_notifier.dart';
 import 'package:flutter/services.dart';
+import 'webview.dart';
 
 
 void main() {
@@ -14,155 +15,67 @@ void main() {
         child:  MyApp(),
       ));
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Webpage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class Webpage extends StatefulWidget {
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
   @override
-  State<Webpage> createState() => _WebpageState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _WebpageState extends State<Webpage> {
-  dynamic position=1;
-  final _key = UniqueKey();
-  late WebViewController _controll;
-
-  final GlobalKey<ScaffoldState>_globalKey=GlobalKey<ScaffoldState>();
-
-  doneLoading(String A) {
-    setState(() {
-      position = 0;
-    });
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      const Duration(seconds: 2), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Webpage())),
+    );
   }
-
-  startLoading(String A){
-    setState(() {
-      position = 1;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.transparent,
-          systemNavigationBarDividerColor: Colors.transparent,
-          systemNavigationBarIconBrightness: Brightness.dark,
-          statusBarColor: Color.fromRGBO(20, 95, 142, 1), //or set color with: Color(0xFF0000FF)
-        )
-    );
+    return Container(
+      color: Colors.white,
 
-    return WillPopScope(
-      onWillPop: _onBack,
-      child: Scaffold(
-        key: _globalKey,
-        appBar: EmptyAppBar(),
-        body: IndexedStack(
-          index: position,
-          children: <Widget>[
-            WebView(
-              key: _key,
-              initialUrl: "https://www.tufidcoschemes.co.in/admin",
-              javascriptMode: JavascriptMode.unrestricted,
-              onPageFinished: doneLoading,
-              onPageStarted: startLoading,
-              onWebViewCreated: ( webViewController) {
-                _controll=webViewController;
-              },
-              javascriptChannels: <JavascriptChannel>{
-                _toasterJavascriptChannel(context),
-              },
-              navigationDelegate: (NavigationRequest request) {
-                print('allowing navigation to $request');
-                return NavigationDecision.navigate;
-              },
-              gestureNavigationEnabled: true,
+      child: Center(
+
+
+        child:Column(
+
+          children: [
+            const SizedBox(height: 200),
+            const Padding(padding: EdgeInsets.only(bottom:50,)),
+            Image.asset(
+                'assets/logo.png',
+                width: 300,
+                height: 150,
             ),
-            Container(
-              color: Colors.white,
-              child: const Center(
-                  child: CircularProgressIndicator()
+            const Padding(padding: EdgeInsets.only(bottom:50,)),
+            const Text("Tamil Nadu Urban Finance And Infrastructure Development Corporation LTD",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(20, 95, 142, 1),
+                  decoration: TextDecoration.none,
               ),
             ),
           ],
         ),
+
       ),
+
     );
   }
-
-  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
-    return JavascriptChannel(
-        name: 'Toaster',
-        onMessageReceived: (JavascriptMessage message) {
-          // ignore: deprecated_member_use
-          Scaffold.of(context).showSnackBar(
-            SnackBar(content: Text(message.message)),
-          );
-        });
-  }
-
-  Future<bool> _onBack() async {
-    bool goBack;
-    var value = await _controll.canGoBack();  // check webview can go back
-    if (value) {
-      _controll.goBack(); // perform webview back operation
-      return false;
-    }
-    else {
-      late BackEventNotifier _notifier;
-      await showDialog(
-          context: _globalKey.currentState!.context,
-          builder: (context) => Consumer<BackEventNotifier>(
-              builder: (context, event, child) {
-                _notifier=event;
-                return AlertDialog(
-                  title: const Text(
-                      'Confirmation ', style: TextStyle(color: Color.fromRGBO(20, 95, 142, 1)
-                  )
-                  ),
-                  content: const Text('Do you want exit app ?'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(false);
-                        event.add(false);
-                        },
-                      child: Text("No"), // No
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        event.add(true);
-                        },
-                      child: Text("Yes"), // Yes
-                    ),
-                  ],
-                );
-              }
-              )
-      );
-      //Navigator.pop(_globalKey.currentState!.context);
-      print("_notifier.isBack ${_notifier.isBack}");
-      return _notifier.isBack;
-    }
-  }
-}
-
-class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
-    );
-  }
-  @override
-  Size get preferredSize => const Size(0.0, 0.0);
 }
---
+++ lib/webview.dart
@@ -0,0 +1,150 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/platform_interface.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
+import 'package:provider/provider.dart';
+import 'backevent_notifier.dart';
+import 'package:flutter/services.dart';
+
+
+class Webpage extends StatefulWidget {
+  @override
+  State<Webpage> createState() => _WebpageState();
+}
+
+class _WebpageState extends State<Webpage> {
+  dynamic position=1;
+  final _key = UniqueKey();
+  late WebViewController _controll;
+
+  final GlobalKey<ScaffoldState>_globalKey=GlobalKey<ScaffoldState>();
+
+  doneLoading(String A) {
+    setState(() {
+      position = 0;
+    });
+  }
+
+  startLoading(String A){
+    setState(() {
+      position = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.transparent,
+          systemNavigationBarDividerColor: Colors.transparent,
+          systemNavigationBarIconBrightness: Brightness.dark,
+          statusBarColor: Color.fromRGBO(20, 95, 142, 1), //or set color with: Color(0xFF0000FF)
+        )
+    );
+
+    return WillPopScope(
+      onWillPop: _onBack,
+      child: Scaffold(
+        key: _globalKey,
+        appBar: EmptyAppBar(),
+        body: IndexedStack(
+          index: position,
+          children: <Widget>[
+            WebView(
+              key: _key,
+              initialUrl: "https://www.tufidcoschemes.co.in/admin",
+              javascriptMode: JavascriptMode.unrestricted,
+              onPageFinished: doneLoading,
+              onPageStarted: startLoading,
+              onWebViewCreated: ( webViewController) {
+                _controll=webViewController;
+              },
+              javascriptChannels: <JavascriptChannel>{
+                _toasterJavascriptChannel(context),
+              },
+              navigationDelegate: (NavigationRequest request) {
+                print('allowing navigation to $request');
+                return NavigationDecision.navigate;
+              },
+              gestureNavigationEnabled: true,
+            ),
+            Container(
+              color: Colors.white,
+              child: const Center(
+                  child: CircularProgressIndicator()
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+
+  Future<bool> _onBack() async {
+    bool goBack;
+    var value = await _controll.canGoBack();  // check webview can go back
+    if (value) {
+      _controll.goBack(); // perform webview back operation
+      return false;
+    }
+    else {
+      late BackEventNotifier _notifier;
+      await showDialog(
+          context: _globalKey.currentState!.context,
+          builder: (context) => Consumer<BackEventNotifier>(
+              builder: (context, event, child) {
+                _notifier=event;
+                return AlertDialog(
+                  title: const Text(
+                      'Confirmation ', style: TextStyle(color: Color.fromRGBO(20, 95, 142, 1)
+                  )
+                  ),
+                  content: const Text('Do you want exit app ?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                        event.add(false);
+                      },
+                      child: Text("No"), // No
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        event.add(true);
+                      },
+                      child: Text("Yes"), // Yes
+                    ),
+                  ],
+                );
+              }
+          )
+      );
+      //Navigator.pop(_globalKey.currentState!.context);
+      print("_notifier.isBack ${_notifier.isBack}");
+      return _notifier.isBack;
+    }
+  }
+}
+
+class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+  @override
+  Size get preferredSize => const Size(0.0, 0.0);
+}
